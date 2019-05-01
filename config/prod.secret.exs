e2d46f3e0ee00d6c289647140a16633e6c5f136f
Tom Berman <tjcberman@gmail.com>
2019-05-01 14:15:44 +0100

    Add secret config

---
+++ config/prod.secret.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+
+# You can generate a new secret by running:
+#
+#     mix phx.gen.secret
+config :live_jobs_board, LiveJobsBoardWeb.Endpoint,
+       secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :live_jobs_board, LiveJobsBoard.Repo,
+       ssl: true,
+       url: System.get_env("DATABASE_URL"),
+       size: String.to_integer(System.get_env("POOL_SIZE") || "10")
